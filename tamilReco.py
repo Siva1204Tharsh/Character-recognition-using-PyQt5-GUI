2e50de78469ced8b3916ea91d4fded05b2f4f795
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-09-08 22:09:38 +0530

    h

--- tamilReco.py
+++ tamilReco.py
@@ -118,7 +118,77 @@ class Ui_MainWindow(object):
         label2=label[result.argmax()]
         print(label2)
         self.textEdit.setText(label2)
+    
+    def trainingFunction(self):
+        self.textEdit.setText("Training under process...")
+        #basic cnn for tamil alphabet
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', input_shape=(128,128, 1)))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(BatchNormalization())
+        model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(BatchNormalization())
+        model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(BatchNormalization())
+        model.add(Conv2D(96, kernel_size=(3,3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(BatchNormalization())
+        model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2,2)))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.2))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.3)) # over fitting  the prevention
+        model.add(Dense(247, activation = 'softmax'))
 
+        model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+
+
+        train_datagen = ImageDataGenerator(rescale = None,
+                                           shear_range = 0.2,
+                                           zoom_range = 0.2,
+                                           horizontal_flip = True)
+
+        test_datagen = ImageDataGenerator(rescale = 1./255)
+
+        training_set = train_datagen.flow_from_directory('E:\Data-----Science\DS AI DL ML Project for 30 Days\Character recognition using PyQt5 GUI\Dataset\Train',
+                                                         target_size = (128, 128),
+                                                         batch_size = 32,
+                                                         class_mode = 'categorical')
+        #print(test_datagen);
+        labels = (training_set.class_indices)
+        print(labels)
+        
+
+        test_set = test_datagen.flow_from_directory('E:\Data-----Science\DS AI DL ML Project for 30 Days\Character recognition using PyQt5 GUI\Dataset\test',
+                                                    target_size = (128, 128),
+                                                    batch_size = 32,
+                                                    class_mode = 'categorical')
+
+        labels2 = (test_set.class_indices)
+        print(labels2)
+        #self.textEdit.setText(labels2)
+
+        model.fit_generator(training_set,
+                                 steps_per_epoch = 100,
+                                 epochs = 10,
+                                 validation_data = test_set,
+                                 validation_steps = 125)
+
+
+##        # Part 3 - Making new predictions
+##
+##        model_json=model.to_json()
+##        with open("model.json", "w") as json_file:
+##            json_file.write(model_json)
+##        # serialize weights to HDF5
+##            model.save_weights("model.h5")
+##            print("Saved model to disk")
+##            self.textEdit.setText("Saved model to disk")
     
 
 
